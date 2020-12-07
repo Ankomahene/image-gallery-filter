090bd98743bd6e3b64dbb4bf3b4123aea004a2a2
ankomahene <ankomaheneshadrack@gmail.com>
2020-12-07 19:39:00 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import images from './data';
+import { SRLWrapper } from 'simple-react-lightbox';
+
+const options = {
+	// settings: {
+	// 	overlayColor: 'rgb(25, 136, 124)',
+	// 	autoplaySpeed: 1500,
+	// 	transitionSpeed: 900
+	// },
+	// buttons: {
+	// 	backgroundColor: 'red',
+	// 	iconColor: 'rgba(126, 172, 139, 0.8)'
+	// },
+	// caption: {
+	// 	captionColor: '#a6cfa5',
+	// 	captionFontFamily: 'Raleway, sans-serif',
+	// 	captionFontWeight: '300',
+	// 	captionTextTransform: 'uppercase'
+	// },
+	// progressBar: {
+	// 	height: '20px',
+	// 	fillColor: 'blue',
+	// 	backgroundColor: 'white'
+	// }
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [tag, setTag] = useState('all');
+	const [filteredImages, setFilteredImages] = useState([]);
+
+	useEffect(
+		() => {
+			tag === 'all' ? setFilteredImages(images) : setFilteredImages(images.filter(image => image.tag === tag));
+		},
+		[tag]
+	);
+
+	return (
+		<div className="App">
+			<div className="tags">
+				<TagButton name="all" tagActive={tag === 'all' ? true : false} handleSetTag={setTag} /> /
+				<TagButton name="new" tagActive={tag === 'new' ? true : false} handleSetTag={setTag} /> /
+				<TagButton name="free" tagActive={tag === 'free' ? true : false} handleSetTag={setTag} /> /
+				<TagButton name="pro" tagActive={tag === 'pro' ? true : false} handleSetTag={setTag} />
+			</div>
+			<SRLWrapper options={options}>
+				<div className="container">
+					{filteredImages.map(image => (
+						<div key={image.id} className="image-card">
+							<a href={`/images/${image.imageName}`}>
+								<img className="image" src={`/images/${image.imageName}`} alt="" />
+							</a>
+						</div>
+					))}
+				</div>
+			</SRLWrapper>
+		</div>
+	);
 }
+
+const TagButton = ({ name, handleSetTag, tagActive }) => {
+	return (
+		<button className={`tag ${tagActive ? 'active' : null}`} onClick={() => handleSetTag(name)}>
+			{name.toUpperCase()}
+		</button>
+	);
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,18 @@
+export default [
+	{ id: '1', imageName: 'img1.jpeg', tag: 'free' },
+	{ id: '2', imageName: 'img2.jpeg', tag: 'new' },
+	{ id: '3', imageName: 'img3.jpeg', tag: 'pro' },
+	{ id: '4', imageName: 'img4.jpeg', tag: 'pro' },
+	{ id: '5', imageName: 'img5.jpeg', tag: 'free' },
+	{ id: '6', imageName: 'img6.jpeg', tag: 'new' },
+	{ id: '7', imageName: 'img6.jpeg', tag: 'pro' },
+	{ id: '8', imageName: 'img8.jpeg', tag: 'free' },
+	{ id: '9', imageName: 'img9.jpeg', tag: 'new' },
+	{ id: '10', imageName: 'img10.jpeg', tag: 'new' },
+	{ id: '11', imageName: 'img11.jpeg', tag: 'new' },
+	{ id: '12', imageName: 'img12.jpeg', tag: 'new' },
+	{ id: '13', imageName: 'img13.jpeg', tag: 'free' },
+	{ id: '14', imageName: 'img14.jpeg', tag: 'pro' },
+	{ id: '15', imageName: 'img15.jpeg', tag: 'free' },
+	{ id: '16', imageName: 'img16.jpeg', tag: 'new' }
+];
